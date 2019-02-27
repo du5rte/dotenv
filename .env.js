afe5c96c73b6370a29e5d4817177397ca07a10cd
Duarte Monteiro <du5rte@users.noreply.github.com>
2019-02-27 21:06:24 +0000

    remove unused code

--- .env.js
+++ .env.js
@@ -1,16 +1,3 @@
-function delay(val) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(() => {
-      resolve(val)
-    }, 1000)
-  })
-}
-
-
-// module.exports = Promise.resolve(delay({
-//   JS_BASIC: 'basic',
-// }).then((rs) => rs))
-
 module.exports = {
   JS_BASIC: 'js_basic',
   // JS_PROMISE: 'my js promise'
